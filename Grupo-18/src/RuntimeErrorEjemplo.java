b777054931796e012da31a115ee59971d6235980
kailarodriguez <kaila.rodriguez@estudiantes.utec.edu.uy>
2026-06-14 16:53:34 -0300

    Runtime ejemplo

---
+++ Grupo-18/src/RuntimeErrorEjemplo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class RuntimeErrorEjemplo {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese su edad: ");
+        int edad = sc.nextInt();
+
+        System.out.println("Su edad es: " + edad);
+    }
+}
